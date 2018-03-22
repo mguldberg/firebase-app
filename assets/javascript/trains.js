ec0f398912893d1bc5dfe8731a150508bf883c2e
mguldberg <guldberg@sbcglobal.net>
2018-03-21 22:35:16 -0500

    all required functionality is in the basic-recommended assignment

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -19,26 +19,27 @@ $(document).ready(function () {
 
     // Create a variable to reference the database.
     var database = firebase.database();
-
-    // --------------------------------------------------------------
-    // Link to Firebase Database for viewer tracking
-    // --------------------------------------------------------------
-    // Initial Values
-
+    
+    // Initial Values for DB records
     var trainName = "";
     var trainDestination = "";
     var firstTrainTime = "0";
     var trainFrequency = "30";
 
-    // --------------------------------------------------------------
-    // Add ourselves to presence list when online.
-    // -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-    // connectionsRef references a specific location in our database.
-    // All of our connections will be stored in this directory.
-    var database = firebase.database();
+    //init variables for countdown timer
+    var countDownToNextTrain = 60;
+    var percentComplete = 0;
 
+    //Vars used to trigger a DB update so the minutes until the next train will update
+    var dummyParmUsedForScreenRefresh = 0;
+    var keyForDummyUpdate = "";
+    
+    //let up references to where the DB records will be set
     var trainScheduleRef = database.ref("/dbTrainRecord");
     var newTrainRef = trainScheduleRef.push();
+
+    //set countdown timer function to run every second
+    var countDownTimerId = setInterval(countDownTimerHandler, 1000);
 
     // ----------------------------------------------------------------
     // At the page load and subsequent value changes, get a snapshot of the local data.
@@ -46,21 +47,28 @@ $(document).ready(function () {
     // ----------------------------------------------------------------
     database.ref().on("value", function (snapshot) {
 
-
         // console.log(childSnapshot.val())
         console.log(snapshot.val())
 
+        //var to point inside the snapshot to our train information
         var trainRecords = snapshot.val().dbTrainRecord;
         console.log(snapshot.val().dbTrainRecord);
 
+        //used for console logging to make sure the loop is incrementing
         var loop = 0;
+
+        //empty the table so we can rerender it with current information
         var tableVar = $("#train-table-body");
         tableVar.empty();
 
+        //loop through all objects in the snapshot object returned on value event
         for (key in trainRecords) {
+            console.log("in for loop");
+
+            //validate # of records in the DB
             loop++;
             console.log(loop);
-            console.log("in for loop");
+            keyForDummyUpdate = key;
             console.log(key);
             console.log(trainRecords[key]);
             console.log(trainRecords[key].trainname);
@@ -69,36 +77,38 @@ $(document).ready(function () {
             console.log(trainRecords[key].firsttraintime);
 
             var timeRightNow = moment();
-            var trainTimeconv = moment(trainRecords[key].firsttraintime);
-            
-            console.log(moment(trainTimeconv).format("HH:mm"));
+            var trainTimeConv = moment(trainRecords[key].firsttraintime, "HH:mm");
 
-            var currentTime = moment();
+            console.log(trainTimeConv);
 
-            // // Difference between the times
-            var diffTime = currentTime.diff(moment(trainTimeconv), "minutes");
+            // Difference between the times
+            var diffTime = timeRightNow.diff(moment(trainTimeConv), "minutes");
             console.log("DIFFERENCE IN TIME: " + diffTime);
 
-            var remainder = Math.abs(diffTime) % frequency;
+            //find the minutes ago the last train left
+            var remainder = Math.abs(diffTime) % trainRecords[key].trainfrequency;
             console.log(remainder);
 
-            var timeNextTrain = currentTime.add(minNextTrain, "minutes")
-            console.log("ARRIVAL TIME: " + moment(timeNextTrain).format("hh:mm"));
+            //find minutes until the next train
+            var minUntilNextTrain = trainRecords[key].trainfrequency - remainder;
+            console.log("mins until next train: " + minUntilNextTrain);
 
-            var minNextTrain = frequency - remainder;
-            console.log("MINUTES TILL TRAIN: " + minNextTrain);
+            //find 
+            var nextTrain = timeRightNow.add(minUntilNextTrain, "minutes")
+            console.log("new arrival time: " + moment(nextTrain).format("HH:mm"));
 
             //----------------------------------------------------------------------
             // trainname  /  traindestination  /  trainfrequency  / firsttraintime /
             //----------------------------------------------------------------------
             //
-         
-            var tableRow = $("<tr>");
-            tableRow.append('<td>' + trainRecords[key].trainname + '</td>')
-            tableRow.append("<td>" + trainRecords[key].traindestination + "</td>")
-            tableRow.append("<td>" + trainRecords[key].trainfrequency + "</td>")
 
-            tableRow.append("<td>" + trainRecords[key].firsttraintime + "</td>")
+            var tableRow = $("<tr>");
+            tableRow.append('<td>' + trainRecords[key].trainname + '</td>');
+            tableRow.append("<td>" + trainRecords[key].traindestination + "</td>");
+            tableRow.append("<td>" + trainRecords[key].trainfrequency + "</td>");
+            tableRow.append("<td>" + moment(nextTrain).format("HH:mm") + "</td>");
+            tableRow.append("<td>" + minUntilNextTrain + "</td>");
+
             tableVar.append(tableRow);
 
         }
@@ -129,9 +139,61 @@ $(document).ready(function () {
             trainname: trainName,
             traindestination: trainDestination,
             firsttraintime: firstTrainTime,
-            trainfrequency: trainFrequency
+            trainfrequency: trainFrequency,
+            dummyParmUsedForScreenRefresh: dummyParmUsedForScreenRefresh
         })
 
     });
+
+    //check if time has expired to answer a question
+    function countDownTimerHandler() {
+        // write timer change to the screen - added so 25 will display on the countdown
+        $("#countdown-time").text(countDownToNextTrain);
+        $("#percent-progress-until-next-train").attr("style", "width:" + percentComplete + "%");
+
+        countDownToNextTrain--; // deduct 1 second
+        console.log(countDownToNextTrain);
+
+        //Leverage countdown to set the % complete progress bar (Bootstrap)
+        //percent is counting up vs countDownToNextTrain is counting down
+        // the 100/60 is the % to increment each second of the countdown then write it to the HTML progress bar
+        percentComplete = (100 / 60) * (60 - countDownToNextTrain);
+        console.log("increment: " + (100 / 60) * (60 - countDownToNextTrain));
+        console.log("%:" + percentComplete);
+        console.log("should count from 0 to 100: " + (100 / 60) * (60 - countDownToNextTrain));
+        $("#percent-progress-until-next-train").attr("style", "width:" + percentComplete + "%");
+
+        // write timer change to the screen
+        $("#countdown-time").text(countDownToNextTrain);
+
+        //is timer at 0?
+        //  if yes - reset the timer
+        if (countDownToNextTrain == 0) {
+            console.log("inside timer = 0");
+
+            //reset timer to 60 seconds
+            countDownToNextTrain = 60;
+            percentComplete = 0;
+
+
+            //Write a dummy update to the DB to trigger a value event to redraw the table and thereby update the 
+            //the arrival times of the next train
+            //https://firebase.google.com/docs/database/web/read-and-write
+
+            // var newTrainRef = trainScheduleRef;
+
+            //key is set up above from last loop through the DB on a redraw
+            console.log(keyForDummyUpdate);
+
+            //force a delta in the DB
+            dummyParmUsedForScreenRefresh++;
+
+            var updates = {};
+            updates['/dbTrainRecord/' + keyForDummyUpdate + '/dummyParmUsedForScreenRefresh'] = dummyParmUsedForScreenRefresh;
+            database.ref().update(updates);
+
+
+        };
+    };
 
 })
